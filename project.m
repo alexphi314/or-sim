e0a5a7cdfe4f94c083ff85e6cafd56ce4c81f990
Alex Philpott <alex_philpott@icloud.com>
2017-12-09 19:45:55 -0500

    Updated simulation to include period after flyby

--- project.m
+++ project.m
@@ -118,32 +118,29 @@ OR_x0 = E_OR(1,:); %km
 OR_v0 = VE_OR(1,:); %km/s
 
 figure;axis equal;  hold on; %view(3);
-%h1 = plot(Sun_pos(:,1),Sun_pos(:,2),'r','DisplayName','Sun');
-%plot(Sun_x0(1),Sun_x0(2),'r*');
-%plot(Sun_pos(end,1),Sun_pos(end,2),'ro');
 plot3(Earth_pos(:,1),Earth_pos(:,2),Earth_pos(:,3),'b','DisplayName','Earth');
 %plot3(E_Earth(1:379,1),E_Earth(1:379,2),E_Earth(1:379,3),':r','DisplayName','JPL Earth');
 %plot(Earth_x0(1),Earth_x0(2),'b*');
 % plot(Earth_pos(end,1),Earth_pos(end,2),'r*');
-% %plot(Jupiter_pos(:,1),Jupiter_pos(:,2),'r');
-% %plot(E_Jupiter(1:379,1),E_Jupiter(1:379,2),'--b');
 %h3 = plot3(Bennu_pos(:,1),Bennu_pos(:,2),Bennu_pos(:,3),'k','DisplayName','Bennu');
 %plot3(E_Bennu(1:379,1),E_Bennu(1:379,2),E_Bennu(1:379,3),'--r');
 % %plot3(Bennu_x0(1),Bennu_x0(2),'k*');
 % %plot3(Bennu_pos(end,1),Bennu_pos(end,2),'ko');
 h4 = plot3(OR_pos(:,1),OR_pos(:,2),OR_pos(:,3),'g','DisplayName','Osiris Rex');
 % plot(OR_pos(145,1),OR_pos(145,2),'b*');
-h5 = plot3(E_OR(1:379,1),E_OR(1:379,2),E_OR(1:379,3),'--k','DisplayName','JPL Osiris Rex');
+h5 = plot3(E_OR(1:ts(3),1),E_OR(1:ts(3),2),E_OR(1:ts(3),3),'--k','DisplayName','JPL Osiris Rex');
 % plot(OR_x0(1),OR_x0(2),'go');
 % plot(OR_pos(end,1),OR_pos(end,2),'g*');
 % legend([h2 h3 h4 h5],'Location','Northwest');
 
-print('orbits','-dpng');
+figure; hold on;
+plot3(OR_pos(ts(3):end,1),OR_pos(ts(3):end,2),OR_pos(ts(3):end,3),'g','DisplayName','Osiris Rex');
+plot3(E_OR(ts(3):ts(4),1),E_OR(ts(3):ts(4),2),E_OR(ts(3):ts(4),3),'--k','DisplayName','JPL Osiris Rex');
 
-figure; hold on; axis equal;
-plot(E_OR(:,1),E_OR(:,2),'g');
-plot(E_Earth(:,1),E_Earth(:,2),'b');
-plot(E_Bennu(:,1),E_Bennu(:,2),'k');
+% figure; hold on; axis equal;
+% plot(E_OR(:,1),E_OR(:,2),'g');
+% plot(E_Earth(:,1),E_Earth(:,2),'b');
+% plot(E_Bennu(:,1),E_Bennu(:,2),'k');
 
 %% Analyzing
 r = zeros(size(OR_pos,1),1);
@@ -167,3 +164,9 @@ end
 
 [burn_amnt,day] = max(v(:,4));
 fprintf('delta-V %% of %.3f found on Day %i.\n',burn_amnt,day);
+
+%Grabbing vectors
+before_burn = VE_OR(379,:);
+after_burn = VE_OR(380,:);
+bb_u = before_burn./norm(before_burn);
+ab_u = after_burn./norm(after_burn);
